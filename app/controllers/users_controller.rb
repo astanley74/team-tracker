e9af5d59aa1ccd4722377af8c656525fdff15c35
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-12-19 19:06:11 -0500

    add create action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,28 @@
 class UsersController < ApplicationController
 
-    def show
-    end
-
     def new
         @user = User.new
     end
 
     def create
+
         #create user
         #if user is saved
             # redirect to user show page
         #else
             # give error messages and redirect to signup page
         #end 
+
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render 'users/new'
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
     end
 
     private
