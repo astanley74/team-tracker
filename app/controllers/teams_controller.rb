d0d7d39817d16ee77d4df3055b20c61978317b6d
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-12-22 01:25:03 -0500

    add strong params to teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,5 +3,9 @@ class TeamsController < ApplicationController
         @team = Team.new
     end
 
-    
+    private
+        def team_params
+            params.require(:team).permit(:city, :name, :coach, :user_id, :league_id)
+        end
+
 end
