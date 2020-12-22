10a3162b06ed63d20fad5d8b158e00c6a39d329b
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-12-22 16:06:23 -0500

    add signup get and post route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   resources :teams
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
