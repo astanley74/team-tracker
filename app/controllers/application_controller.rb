8b308a0ba5658d1ea59f698c5827c774b3b84cee
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-12-22 12:21:56 -0500

    add current_user helper method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
 
+    helper_method :current_user
+
     def home
     end
+
+    private
+        def current_user
+            @current_user ||= User.find_by_id(session[:user_id])
+        end
 
 end
