278dd83ebaa0b663a2f7c174fa1bff2f8f5850d8
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-12-23 01:00:15 -0500

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
     has_many :teams
     has_many :leagues, through: :teams
     has_secure_password
+    validates_presence_of :email, :password, :name
+    validates :email, uniqueness: true
+    validates :password, length: {minimum: 5}
 end
