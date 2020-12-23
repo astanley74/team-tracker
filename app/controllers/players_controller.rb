f3630fec9fde8917f4f340286210e12af9744587
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-12-22 21:19:05 -0500

    add create action to players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,7 +5,15 @@ class PlayersController < ApplicationController
     end
 
     def create
+        @player = Player.new(player_params)
+        if @player.save
+            redirect_to team_player_path(@player)
+        else
+            redirect_to new_team_player_path
+        end
     end
+
+
 
     private
         def player_params
