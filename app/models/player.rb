49b1e7b5b9eee1c741dae40f4986bdee82a9c800
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-12-23 14:18:25 -0500

    add validations to player and team model

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,5 @@ class Player < ActiveRecord::Base
     belongs_to :team
     validates_presence_of :name, :number, :hometown
     validates :name, uniqueness: true
+    validates :number, uniqueness: {scope: :team_id}
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,7 @@ class Team < ActiveRecord::Base
     has_many :players
     belongs_to :user
     belongs_to :league
+
+    validates_presence_of :city, :name, :coach
+    validates :name, :coach, uniqueness: true
 end
