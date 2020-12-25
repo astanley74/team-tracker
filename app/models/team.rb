bcff3e1d09763b2cc2ed8c6bf56902c52e93cadb
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-12-25 01:01:09 -0500

    add scope method to team model and refactor users controller

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,4 +6,10 @@ class Team < ActiveRecord::Base
     validates_presence_of :city, :name, :coach
     validates :city, uniqueness: {scope: :league_id}
     validates :name, :coach, uniqueness: true
+
+    # def self.order_by_name
+    #     order(name: :desc)
+    # end
+
+    scope :order_by_name, -> { order(name: :desc) }
 end
