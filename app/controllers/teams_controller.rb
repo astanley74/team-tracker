242ac716a0d9ebc7716abd4968d25fdd7dd52041
astanley74 <stanley.377@buckeyemail.osu.edu>
2020-12-25 02:46:15 -0500

    refactor update and destroy action in teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,13 +1,9 @@
 class TeamsController < ApplicationController
     before_action :verified_user
+    before_action :users_team, only: [:new, :edit, :update]
 
     def new
-        if current_user == User.find(params[:user_id])
-            @team = current_user.teams.build
-        else
-            redirect_to user_path(current_user)
-            flash[:error] = "You must be the correct user to add a team"
-        end
+        @team = current_user.teams.build
     end
 
     def create
@@ -25,11 +21,10 @@ class TeamsController < ApplicationController
     end
 
     def edit
-        @team = current_user.teams.find(params[:id])
+        @team = User.find(params[:user_id]).teams.find(params[:id])
     end
 
     def update
-        @team = Team.find(params[:id])
         if @team.update(team_params)
             redirect_to user_team_path(current_user, @team)
         else
@@ -38,13 +33,22 @@ class TeamsController < ApplicationController
     end
 
     def destroy
-        Team.find(params[:id]).destroy
-        redirect_to user_path(current_user)
+        if current_user == Team.find(params[:id]).user
+            Team.find(params[:id]).destroy
+            redirect_to user_path(current_user)
+        else
+            redirect_to user_path(current_user)
+            flash[:error] = "You must be the owner of the team to edit or delete"
+        end
     end
 
     private
         def team_params
             params.require(:team).permit(:city, :name, :coach, :user_id, :league_id)
+        end
+
+        def users_team
+            redirect_to user_path(current_user) unless current_user == User.find(params[:user_id])
         end
 
 end
